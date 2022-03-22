96620f3e7dc5ab0cedd163826700fb9349460333
Leonardo Garreto <leonardogarreto@gmail.com>
2022-03-21 22:11:33 -0300

    trying to solve the grid problem

---
+++ src/components/ContinentStatistics.tsx
@@ -0,0 +1,16 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function ContinentStatistics() {
+  return (
+    <Flex align="center">
+      <Flex align="center" justify="center" direction="column">
+        <Text fontSize="36" lineHeight={1} color="orange.300">
+          50
+        </Text>
+        <Text as="span" fontSize="20" color="gray.800">
+          países
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,5 +1,6 @@
-import { Flex, Image, Text, Box } from "@chakra-ui/react";
+import { Flex, Image, Text, Box, Grid } from "@chakra-ui/react";
 import React from "react";
+import ContinentStatistics from "../components/ContinentStatistics";
 
 export default function Continent() {
   return (
@@ -17,7 +18,7 @@ export default function Continent() {
         </Text>
       </Box>
 
-      <Flex w="100%" p="20" justify="space-between">
+      <Flex w="100%" p="20" justify="space-between" gap="20">
         <Box>
           <Text>
             A Europa é, por convenção, um dos seis continentes do mundo.
@@ -26,16 +27,12 @@ export default function Continent() {
             o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste
           </Text>
         </Box>
-        <Flex align="center">
-          <Flex align="center" justify="center" direction="column">
-            <Text fontSize="36" lineHeight={1} color="orange.300">
-              50
-            </Text>
-            <Text as="span" fontSize="20" color="gray.800">
-              países
-            </Text>
-          </Flex>
-        </Flex>
+        <Grid gap="8" templateColumns="repeat(auto-fit, 60px)" minW={400}>
+          <ContinentStatistics />
+          <ContinentStatistics />
+          <ContinentStatistics />
+          <ContinentStatistics />
+        </Grid>
       </Flex>
     </Flex>
   );
