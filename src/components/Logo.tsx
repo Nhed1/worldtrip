bb0d2483f10c9dc2b3ecb3c228292af447e25767
Leonardo Garreto <leonardogarreto@gmail.com>
2022-03-22 21:25:38 -0300

    logo link to "/"

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,15 @@
 import { Center, Image } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function Logo() {
   return (
     <Center h={100}>
-      <Image
-        src="../images/Logo.png"
-        alt="A logo do WorldTrip com um icone de um avião ao lado direito do nome WorldTrip"
-      />
+      <Link href="/">
+        <Image
+          src="../images/Logo.png"
+          alt="A logo do WorldTrip com um icone de um avião ao lado direito do nome WorldTrip"
+        />
+      </Link>
     </Center>
   );
 }
