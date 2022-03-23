1357b6551cb9eb12d00c8ffa79e7ce32b4810f1c
Leonardo Garreto <leonardogarreto@gmail.com>
2022-03-22 21:02:54 -0300

    swipper added

--- src/components/Swipper.tsx
+++ src/components/Swipper.tsx
@@ -1,5 +1,47 @@
-import { Text } from "@chakra-ui/react";
+import { Flex, Text, Image, Box, Center } from "@chakra-ui/react";
+// swipper imports
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 export default function Swipper() {
-  return <Text></Text>;
+  return (
+    <Box h={200} w="80%" m="auto" mt="10" mb="6">
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Center h={200} position="relative">
+            <Image src="./images/europe.jpg" objectFit="cover" />
+            <Text position="absolute" color="white" fontSize="26">
+              Europe
+            </Text>
+          </Center>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Center h={200} position="relative">
+            <Image src="./images/europe.jpg" objectFit="cover" />
+            <Text position="absolute" color="white" fontSize="26">
+              Brazil
+            </Text>
+          </Center>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Center h={200} position="relative">
+            <Image src="./images/europe.jpg" objectFit="cover" />
+            <Text position="absolute" color="white" fontSize="26">
+              Australia
+            </Text>
+          </Center>
+        </SwiperSlide>
+      </Swiper>
+    </Box>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import type { NextPage } from "next";
 import { Flex } from "@chakra-ui/react";
 import Header from "../components/Header";
-import Logo from "../components/Logo";
 import Main from "../components/Main";
 import Swipper from "../components/Swipper";
 
 const Home: NextPage = () => {
   return (
     //Logo 100px
-    <Flex direction="column" h="calc(100vh - 100px)" w="100vw" bg="gray.100">
+    <Flex direction="column" h="fit-content" w="100vw" bg="gray.100">
       <Header />
       <Main />
       <Swipper />
